4b879b6f973438af644418d239296d96e5a397a1
emlunar <emlunar>
2024-08-23 03:18:05 +0000

    re-init with swift-test

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGH",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.11.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
