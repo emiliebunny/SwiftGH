81d99e23e96169d49632c24a42fb35dbcc6c6445
emlunar <emlunar>
2024-08-25 14:29:17 +0000

    re-factor project folder structure and module - init

--- GitCLI/Tests/GitCLITests/GitCLITests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-import XCTest
-@testable import GitCLI
-
-@Test func example() throws {
-    // swift-testing Documentation
-    // https://swiftpackageindex.com/apple/swift-testing/main/documentation/testing
-}
